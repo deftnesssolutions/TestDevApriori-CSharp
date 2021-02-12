3b66a8a4ca0928b68bee9d10cb6318c85a914de0
deftnesssolutions <gusove@gmail.com>
2021-02-12 01:23:26 -0300

    Criação de tela de login

--- Orcamento.Web/Controllers/CadastroController.cs
+++ Orcamento.Web/Controllers/CadastroController.cs
@@ -9,14 +9,17 @@ namespace Orcamento.Web.Controllers
     public class CadastroController : Controller
     {
         // GET: Cadastro
+        [Authorize]
         public ActionResult Cliente()
         {
             return View();
         }
+        [Authorize]
         public ActionResult Produto()
         {
             return View();
         }
+        [Authorize]
         public ActionResult Orcamento()
         {
             return View();
