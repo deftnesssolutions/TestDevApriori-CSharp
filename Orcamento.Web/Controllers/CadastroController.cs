71b11d006be27d53c79ca6fd8b18cfab5f1866c2
deftnesssolutions <gusove@gmail.com>
2021-02-12 17:58:19 -0300

    Criação CRUD Produto Web

--- Orcamento.Web/Controllers/CadastroController.cs
+++ Orcamento.Web/Controllers/CadastroController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Orcamento.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,17 +9,70 @@ namespace Orcamento.Web.Controllers
 {
     public class CadastroController : Controller
     {
-        // GET: Cadastro
+
+        private static List<ProdutoViewModel> _listaProduto = new List<ProdutoViewModel>()
+        {
+            new ProdutoViewModel() { Id=1, Nome="Livros", Preco = 10 },
+            new ProdutoViewModel() { Id=2, Nome="Mouses", Preco = 20 },
+            new ProdutoViewModel() { Id=3, Nome="Monitores", Preco = 30 }
+        };
+
         [Authorize]
         public ActionResult Cliente()
         {
             return View();
         }
+
         [Authorize]
         public ActionResult Produto()
         {
-            return View();
+            return View(_listaProduto);
         }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult RecuperarProduto(int id)
+        {
+            return Json(_listaProduto.Find(x => x.Id == id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult ExcluirProduto(int id)
+        {
+            var ret = false;
+
+            var registroBD = _listaProduto.Find(x => x.Id == id);
+            if (registroBD != null)
+            {
+                _listaProduto.Remove(registroBD);
+                ret = true;
+            }
+
+            return Json(ret);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult SalvarProduto(ProdutoViewModel model)
+        {
+            var registroBD = _listaProduto.Find(x => x.Id == model.Id);
+
+            if (registroBD == null)
+            {
+                registroBD = model;
+                registroBD.Id = _listaProduto.Max(x => x.Id) + 1;
+                _listaProduto.Add(registroBD);
+            }
+            else
+            {
+                registroBD.Nome = model.Nome;
+                registroBD.Preco = model.Preco;
+            }
+
+            return Json(registroBD);
+        }
+
         [Authorize]
         public ActionResult Orcamento()
         {
---
+++ Orcamento.Web/Models/ProdutoViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Orcamento.Web.Models
+{
+    public class ProdutoViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Preencha o nome.")]
+        [MaxLength(50, ErrorMessage = "O nome pode ter no máximo 50 caracteres.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Preencha o preço.")]
+        public decimal Preco { get; set; }
+
+    }
+}
